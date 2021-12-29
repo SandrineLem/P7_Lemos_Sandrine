530718a0d26e3b39ae49987e9c16058151fe9e36
SandrineLem <lemossandrine@gmail.com>
2021-12-29 20:08:17 +0100

    fiste commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+//instanciation server 
+var server = express();
+
+//config routes 
+server.get('/', function (req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>Bonjour le server ca se met a jour tout est ok </h1>');
+});
+
+//ecoute du server 
+server.listen(8080, function(){
+    console.log('server en ecoute')
+});
